18fadb82b9d372800888aca6ef3653de9fd25648
Tecnika <nika.koledova@yandex.ru>
2023-07-20 23:38:16 +0300

    Update MangaCleanInList.user.js

--- Mangalib/MangaCleanInList.user.js
+++ Mangalib/MangaCleanInList.user.js
@@ -5,7 +5,7 @@
 // @grant       none
 // @version     1.0
 // @author      Tecnika
-// @downloadURL https://github.com/Tecnika/MyUserScripts/edit/main/Mangalib/MangaCleanInList.user.js
+// @downloadURL https://github.com/Tecnika/MyUserScripts/edit/raw/Mangalib/MangaCleanInList.user.js
 // ==/UserScript==
 var needclass = ".bookmark-actions";
 window.addEventListener('load', function () {
